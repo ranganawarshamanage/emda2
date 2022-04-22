2e81834caa4b257d3fef808c6bd9fd2027cab985
Rangana Warshamanage <ranganaw@mrc-lmb.cam.ac.uk>
2022-04-22 18:26:48 +0100

    updated

--- emda2/ext/__init__.py
+++ emda2/ext/__init__.py
@@ -6,6 +6,4 @@ This software is released under the
 Mozilla Public License, version 2.0; see LICENSE.
 """
 
-__path__ = __import__('pkgutil').extend_path(__path__, __name__)
-
-#import fcodes_fast
+import fcodes2
