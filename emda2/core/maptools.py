1b8f36e6d8181e439ceb27b2c40bee89cf83233d
Rangana Warshamanage <ranganaw@mrc-lmb.cam.ac.uk>
2022-11-29 18:01:53 +0000

    copyright added

--- emda2/core/maptools.py
+++ emda2/core/maptools.py
@@ -1,3 +1,11 @@
+"""
+Author: "Rangana Warshamanage, Garib N. Murshudov"
+MRC Laboratory of Molecular Biology
+
+This software is released under the
+Mozilla Public License, version 2.0; see LICENSE.
+"""
+
 import numpy as np
 import fcodes2
 import emda2.core.iotools as iotools
