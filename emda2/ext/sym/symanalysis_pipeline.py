6f3ba273443025127e6be8a29353e9dee4a9355f
Rangana Warshamanage <ranganaw@mrc-lmb.cam.ac.uk>
2022-11-29 18:10:36 +0000

    copyright info added

--- emda2/ext/sym/symanalysis_pipeline.py
+++ emda2/ext/sym/symanalysis_pipeline.py
@@ -1,3 +1,11 @@
+
+"""
+Author: "Rangana Warshamanage, Garib N. Murshudov"
+MRC Laboratory of Molecular Biology
+
+This software is released under the
+Mozilla Public License, version 2.0; see LICENSE.
+"""
 # Symmetry ananlysis and pointgroup detection
 # Halfmaps are required
 # Compute FSC-hf --> FSC-full
