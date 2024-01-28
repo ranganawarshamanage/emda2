66eb48ceb18080965915f75dcaee72346b74e5ac
Rangana Warshamanage <rangawms@gmail.com>
2024-01-28 19:06:39 +0000

    updated args

--- tests/test_symmetry.py
+++ tests/test_symmetry.py
@@ -1,17 +1,31 @@
-import emda2.emda_methods2 as em
+# Test code to validate/find the point group of an EM map
 
-half1 = "/Users/ranganaw/MRC/REFMAC/symmetry/EMD-21231/emd_21231_half_map_1.map"
-mask = "/Users/ranganaw/MRC/REFMAC/symmetry/EMD-21231/emd_21231_msk_1.map"
-resol = 6.0
+import emda2.emda_methods2 as em
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='EMDA Point group validation')
+parser.add_argument(
+    '--half1name', type=str, required=True, help='half1 file mrc/map')
+parser.add_argument(
+    '--half2name', type=str, required=True, help='half2 file mrc/map')
+parser.add_argument(
+    '--maskname', type=str, required=True, help='mask file mrc/map')
+parser.add_argument(
+    '--resolution', type=float, required=True, help='resolution (A)')
+args = parser.parse_args()
 
 try:
-    (proshade_pg, 
-    emda_pg, 
-    maskname, 
-    reboxedmapname) = em.get_pointgroup(
-        half1=half1,
-        mask=mask,
-        resol=float(resol)
+    (
+        proshade_pg,
+        emda_pg,
+        maskname,
+        reboxedmapname
+    ) = em.get_pointgroup(
+        half1=args.half1name,
+        half2=args.half2name,
+        mask=args.maskname,
+        resol=float(args.resolution)
     )
 except ValueError as e:
     print(e)
