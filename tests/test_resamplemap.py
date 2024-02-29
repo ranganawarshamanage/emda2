87d6a64210f2126f84c2080c20ff6bcc19d76745
Rangana Warshamanage <rangawms@gmail.com>
2024-02-29 10:16:52 +0000

    target dim modified

--- tests/test_resamplemap.py
+++ tests/test_resamplemap.py
@@ -13,13 +13,13 @@ mapobj1 = iotools.Map(name=args.mapname)
 mapobj1.read()
 
 if args.target_dim is None:
-    target_dim = list(mapobj1.shape)
+    target_dim = list(mapobj1.workarr.shape)
 else:
     target_dim = args.target_dim
 
-resampled_arr  = em.resample_data(curnt_pix=[mapobj1.cell[i]/shape for i, shape in enumerate(mapobj1.arr.shape)],
+resampled_arr  = em.resample_data(curnt_pix=[mapobj1.workcell[i]/shape for i, shape in enumerate(mapobj1.workarr.shape)],
                                  targt_pix=[args.target_pix,args.target_pix,args.target_pix],
-                                 arr=mapobj1.arr,
+                                 arr=mapobj1.workarr,
                                  targt_dim=target_dim,
                                   )
 
